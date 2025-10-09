67b29d841a4d6b00eb69beea9235e39a91556e79
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-09 13:23:03 +0200

    Updated colors.dart

---
+++ lib/design/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// green: FF25AA68
+// grey: FF989898
+// white: FFF1F1F1
+// blue: FF648DDB
+
+// inputpanel grey: FF2B2B38
+// inputpanelText grey: FF4A4B56
+
+//background: FF161621
+
+final ColorScheme customColorScheme = ColorScheme.fromSeed(
+    seedColor: const Color(0xFF25AA68)
+).copyWith(
+  //
+  //
+  // secondaryContainer: Color(0xFF25AA68),
+  // surface: Color(0xFF161621),
+  // surfaceContainer: Color(0xFF2B2B38),
+  // onSurface: Color(0xFFF1F1F1),
+);
