a85c01ac83821d7540573c63b872b8be91c0dbbe
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-10 20:23:17 +0200

    Created dio_provider.dart

---
+++ lib/shared/network/dio_provider.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://postman-echo.com',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    ),
+  );
+
+  dio.interceptors.add(LogInterceptor(responseBody: true));
+
+  return dio;
+});
