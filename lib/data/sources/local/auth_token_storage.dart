504b5c874935edcd8f9b56cc3aa8c5e22d8036ef
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-11 11:20:39 +0200

    Created auth_token_storage.dart using FlutterSecureStorage

---
+++ lib/data/sources/local/auth_token_storage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthTokenStorage {
+  AuthTokenStorage(this._secure);
+  final FlutterSecureStorage _secure;
+
+  static const _tokenKey = 'auth_token';
+
+  Future<void> save(String token) => _secure.write(key: _tokenKey, value: token);
+  Future<String?> read() => _secure.read(key: _tokenKey);
+  Future<void> clear() => _secure.delete(key: _tokenKey);
+}
