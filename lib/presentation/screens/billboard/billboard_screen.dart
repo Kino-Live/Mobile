e6e291fd7a59adbca431a460d228f72afb754fb8
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-12 12:59:53 +0200

    Added function logout to the 3rd button of BottomNavigation (for testing)

--- lib/presentation/screens/billboard/billboard_screen.dart
+++ lib/presentation/screens/billboard/billboard_screen.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:kinolive_mobile/presentation/screens/billboard/billboard_form.dart';
+import 'package:kinolive_mobile/presentation/viewmodels/auth_controller.dart';
+import 'package:kinolive_mobile/shared/providers/auth_provider.dart';
 
-class BillboardScreen extends StatefulWidget {
+class BillboardScreen extends ConsumerStatefulWidget {
   const BillboardScreen({super.key});
 
   @override
-  State<BillboardScreen> createState() => _BillboardScreenState();
+  ConsumerState<BillboardScreen> createState() => _BillboardScreenState();
 }
 
-class _BillboardScreenState extends State<BillboardScreen> {
-  int _index = 0;
+class _BillboardScreenState extends ConsumerState<BillboardScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    ref.listen(authStateProvider, (prev, next) {
+      final wasAuthed = prev?.isAuthenticated == true;
+      final nowAuthed = next.isAuthenticated;
+
+      if (wasAuthed && !nowAuthed) {
+        final messenger = ScaffoldMessenger.of(context);
+        messenger.hideCurrentSnackBar();
+        messenger.showSnackBar(
+          const SnackBar(content: Text('Logged out', textAlign: TextAlign.center,)),
+        );
+      }
+    });
+
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
 
@@ -24,9 +41,7 @@ class _BillboardScreenState extends State<BillboardScreen> {
         centerTitle: true,
         title: Text(
           'KinoLive',
-          style: textTheme.headlineMedium?.copyWith(
-            color: colorScheme.primary,
-          ),
+          style: textTheme.headlineMedium?.copyWith(color: colorScheme.primary),
         ),
         actions: [
           Padding(
@@ -42,12 +57,22 @@ class _BillboardScreenState extends State<BillboardScreen> {
           ),
         ],
       ),
-      body: SafeArea(
-        child: BillboardForm()
-      ),
+      body: const SafeArea(child: BillboardForm()),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
-        onTap: (i) => setState(() => _index = i),
+        currentIndex: _currentIndex,
+        onTap: (index) async {
+          setState(() => _currentIndex = index);
+
+          switch (index) {
+            case 0:
+              break;
+            case 1:
+              break;
+            case 2:
+              await ref.read(authStateProvider.notifier).logout();
+              break;
+          }
+        },
         backgroundColor: Colors.transparent,
         elevation: 0,
         selectedItemColor: colorScheme.primary,
