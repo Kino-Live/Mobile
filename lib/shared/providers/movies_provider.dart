158fadc57599ecc14c07d0ff700866106e75694f
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-13 13:45:05 +0200

    Created movies_provider.dart

---
+++ lib/shared/providers/movies_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kinolive_mobile/data/repositories/movies_repository_impl.dart';
+import 'package:kinolive_mobile/data/sources/remote/movie_api_service.dart';
+import 'package:kinolive_mobile/domain/repositories/movies_repository.dart';
+import 'package:kinolive_mobile/domain/usecases/billboard/get_now_showing_movies.dart';
+import 'package:kinolive_mobile/shared/providers/network/dio_provider.dart';
+
+final moviesApiServiceProvider = Provider<MoviesApiService>((ref) {
+  return MoviesApiService(ref.read(dioProvider));
+});
+
+final moviesRepositoryProvider = Provider<MoviesRepository>((ref) {
+  return MoviesRepositoryImpl(ref.read(moviesApiServiceProvider));
+});
+
+final getNowShowingMoviesProvider = Provider<GetNowShowingMovies>((ref) {
+  return GetNowShowingMovies(ref.read(moviesRepositoryProvider));
+});
