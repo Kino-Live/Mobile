a6ae389b1eeed15b88b0152131927c3ee6616baa
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-13 13:07:52 +0200

    Created movie entity (Created movie.dart)

---
+++ lib/domain/entities/movie.dart
@@ -0,0 +1,13 @@
+class Movie {
+  final int id;
+  final String title;
+  final String posterUrl;
+  final double rating;
+
+  const Movie({
+    required this.id,
+    required this.title,
+    required this.posterUrl,
+    required this.rating,
+  });
+}
