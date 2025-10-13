faa06b2e6b2d9a0a92d5dade10e4ef3c1f15d161
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-13 16:36:52 +0200

    Updated movies_provider.dart

--- lib/shared/providers/movies_provider.dart
+++ lib/shared/providers/movies_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kinolive_mobile/data/repositories/movies_repository_impl.dart';
 import 'package:kinolive_mobile/data/sources/remote/movie_api_service.dart';
+import 'package:kinolive_mobile/domain/entities/movie_details.dart';
 import 'package:kinolive_mobile/domain/repositories/movies_repository.dart';
 import 'package:kinolive_mobile/domain/usecases/billboard/get_now_showing_movies.dart';
+import 'package:kinolive_mobile/domain/usecases/movie_details/get_movie_details.dart';
 import 'package:kinolive_mobile/shared/providers/network/dio_provider.dart';
 
 final moviesApiServiceProvider = Provider<MoviesApiService>((ref) {
@@ -15,4 +17,14 @@ final moviesRepositoryProvider = Provider<MoviesRepository>((ref) {
 
 final getNowShowingMoviesProvider = Provider<GetNowShowingMovies>((ref) {
   return GetNowShowingMovies(ref.read(moviesRepositoryProvider));
+});
+
+final getMovieDetailsProvider = Provider<GetMovieDetails>((ref) {
+  return GetMovieDetails(ref.read(moviesRepositoryProvider));
+});
+
+final movieDetailsFutureProvider =
+FutureProvider.family<MovieDetails, int>((ref, id) {
+  final usecase = ref.read(getMovieDetailsProvider);
+  return usecase(id);
 });
