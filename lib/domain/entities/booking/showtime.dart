56c6eb787355e86d6c915de4665285332a238541
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-29 12:40:15 +0100

    Created showtime.dart

---
+++ lib/domain/entities/booking/showtime.dart
@@ -0,0 +1,19 @@
+class Showtime {
+  final String id;
+  final int movieId;
+  final String date;
+  final String quality; // "2D" or "3D"
+  final String startIso;
+  final String endIso;
+  final int hallId;
+
+  const Showtime({
+    required this.id,
+    required this.movieId,
+    required this.date,
+    required this.quality,
+    required this.startIso,
+    required this.endIso,
+    required this.hallId,
+  });
+}
