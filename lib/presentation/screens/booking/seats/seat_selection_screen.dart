891e9b7ef911f66f20458505c9b9acf4cdbea4d3
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-10-30 12:30:20 +0100

    Created files seat_selection_screen.dart and seat_selection_form.dart

---
+++ lib/presentation/screens/booking/seats/seat_selection_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SeatSelectionScreen extends ConsumerStatefulWidget {
+  const SeatSelectionScreen({super.key, required this.movieId});
+  final String movieId;
+
+  @override
+  ConsumerState<SeatSelectionScreen> createState() => _SeatSelectionScreenState();
+}
+
+class _SeatSelectionScreenState extends ConsumerState<SeatSelectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
