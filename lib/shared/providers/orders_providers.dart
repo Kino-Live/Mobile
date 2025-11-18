a984adf850d6c5af2d84c9bc5954531e0bcecd75
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-11-18 13:25:16 +0100

    Created orders_providers.dart

---
+++ lib/shared/providers/orders_providers.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kinolive_mobile/data/repositories/orders_repository_impl.dart';
+import 'package:kinolive_mobile/data/sources/remote/orders_api_service.dart';
+import 'package:kinolive_mobile/domain/repositories/orders_repository.dart';
+import 'package:kinolive_mobile/domain/usecases/orders/create_order.dart';
+import 'package:kinolive_mobile/domain/usecases/orders/get_my_orders.dart';
+import 'package:kinolive_mobile/shared/providers/network/dio_provider.dart';
+
+final ordersApiServiceProvider = Provider<OrdersApiService>((ref) {
+  final dio = ref.watch(dioProvider);
+  return OrdersApiService(dio);
+});
+
+final ordersRepositoryProvider = Provider<OrdersRepository>((ref) {
+  final api = ref.watch(ordersApiServiceProvider);
+  return OrdersRepositoryImpl(api);
+});
+
+final createOrderProvider = Provider<CreateOrder>((ref) {
+  final repo = ref.watch(ordersRepositoryProvider);
+  return CreateOrder(repo);
+});
+
+final getMyOrdersProvider = Provider<GetMyOrders>((ref) {
+  final repo = ref.watch(ordersRepositoryProvider);
+  return GetMyOrders(repo);
+});
