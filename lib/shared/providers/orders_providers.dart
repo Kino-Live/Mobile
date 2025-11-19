aa0fe6f2f3cab51d27c3f83e2b139c5cbcc22675
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-11-19 14:37:35 +0100

    Updated orders_providers.dart

--- lib/shared/providers/orders_providers.dart
+++ lib/shared/providers/orders_providers.dart
@@ -4,6 +4,7 @@ import 'package:kinolive_mobile/data/sources/remote/orders_api_service.dart';
 import 'package:kinolive_mobile/domain/repositories/orders_repository.dart';
 import 'package:kinolive_mobile/domain/usecases/orders/create_order.dart';
 import 'package:kinolive_mobile/domain/usecases/orders/get_my_orders.dart';
+import 'package:kinolive_mobile/domain/usecases/orders/get_order_details.dart';
 import 'package:kinolive_mobile/shared/providers/network/dio_provider.dart';
 
 final ordersApiServiceProvider = Provider<OrdersApiService>((ref) {
@@ -24,4 +25,9 @@ final createOrderProvider = Provider<CreateOrder>((ref) {
 final getMyOrdersProvider = Provider<GetMyOrders>((ref) {
   final repo = ref.watch(ordersRepositoryProvider);
   return GetMyOrders(repo);
+});
+
+final getOrderDetailsProvider = Provider<GetOrderDetails>((ref) {
+  final repo = ref.watch(ordersRepositoryProvider);
+  return GetOrderDetails(repo);
 });
